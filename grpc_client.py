afd3263f81917bab75c82075b4d09c4955f38c55
Artyom-Blagov <gameysmile94@gmail.com>
2026-01-23 00:15:27 +0200

    5.5 Знакомство с gRPC протоколом

---
+++ grpc_client.py
@@ -0,0 +1,13 @@
+from idlelib.rpc import response_queue
+
+import grpc
+
+import user_service_pb2
+import user_service_pb2_grpc
+
+channel = grpc.insecure_channel('localhost:50051')
+stub = user_service_pb2_grpc.UserServiceStub(channel)
+
+
+response = stub.GetUser(user_service_pb2.GetUserRequest(username="Alex"))
+print(response)
