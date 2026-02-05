f2820b2a2d2a9e435362fb02406abf11a342dbd6
Artyom-Blagov <gameysmile94@gmail.com>
2026-02-06 00:39:08 +0200

    8.3 Маркировки Pytest

--- tools/assertions/users.py
+++ tools/assertions/users.py
@@ -1,7 +1,14 @@
-from clients.users.users_schema import CreateUserResponseSchema, CreateUserRequestSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_equal
 
 def assert_create_user_response(request: CreateUserRequestSchema, response: CreateUserResponseSchema):
+    """
+    Проверяет, что ответ на создание пользователя соответствует запросу.
+
+    :param request: Исходный запрос на создание пользователя.
+    :param response: Ответ API с данными пользователя.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
+    """
     assert_equal(response.user.email, request.email, "email")
     assert_equal(response.user.last_name, request.last_name, "last_name")
     assert_equal(response.user.first_name, request.first_name, "first_name")
