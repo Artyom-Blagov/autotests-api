0790b8f2340c613dbe0516e911e07e369fa18a1b
Artyom-Blagov <gameysmile94@gmail.com>
2026-02-18 18:35:02 +0200

    10.3 Добавление Allure шагов в автотесты. Практика работы с allure.step

--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,3 +1,5 @@
+import allure
+
 from clients.exercises.exercises_schema import (CreateExerciseRequestSchema,
                                                 CreateExerciseResponseSchema,
                                                 ExerciseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema,
@@ -6,7 +8,7 @@ from clients.errors_schema import InternalErrorResponseSchema
 from tools.assertions.base import assert_equal, assert_length
 from tools.assertions.errors import assert_internal_error_response
 
-
+@allure.step("Check exercise")
 def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
     """
     Проверяет, что фактические данные задания соответствуют ожидаемым.
@@ -24,6 +26,7 @@ def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
     assert_equal(actual.description, expected.description, "description")
     assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
 
+@allure.step("Check get exercise response")
 def assert_get_exercise_response(
         actual: GetExerciseResponseSchema,
         expected: ExerciseSchema
@@ -37,6 +40,7 @@ def assert_get_exercise_response(
      """
     assert_exercise(actual.exercise, expected)
 
+@allure.step("Check get exercises response")
 def assert_get_exercises_response(
         get_exercises_response: GetExercisesResponseSchema,
         create_exercise_responses: list[CreateExerciseResponseSchema]
@@ -53,6 +57,7 @@ def assert_get_exercises_response(
     for index, create_exercise_response in enumerate(create_exercise_responses):
         assert_exercise(get_exercises_response.exercises[index], create_exercise_response.exercise)
 
+@allure.step("Check create exercise response")
 def assert_create_exercise_response(
         request: CreateExerciseRequestSchema,
         response: CreateExerciseResponseSchema
@@ -72,6 +77,7 @@ def assert_create_exercise_response(
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
 
+@allure.step("Check update exercise response")
 def assert_update_exercise_response(
         request: UpdateExerciseRequestSchema,
         response: UpdateExerciseResponseSchema
@@ -90,6 +96,7 @@ def assert_update_exercise_response(
     assert_equal(response.exercise.description, request.description, "description")
     assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
 
+@allure.step("Check exercise not found response")
 def assert_exercise_not_found_response(actual: InternalErrorResponseSchema):
     """
     Проверяет, что объект ответа API на запрос содержит внутреннюю ошибку и
